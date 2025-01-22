8e6122a55886e3569c5525e6f626def5e796c8e6
IrvingRobles <dsm21190235.irobles@alumnos.utsv.edu.mx>
2025-01-22 13:00:13 -0600

    commit bd nube

--- model/db.js
+++ model/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',       // dirección de tu servidor MySQL
-    user: 'root',      // usuario de MySQL
-    password: '', // contraseña de MySQL
-    database: 'caltecmex', // Nombre de la base de datos
+    host: 'bygribkuoral2phomtxf-mysql.services.clever-cloud.com',       // dirección de tu servidor MySQL
+    user: 'urddkyk6ocawom8c',      // usuario de MySQL
+    password: 'wRvIpHcxmSAPnTeqL8D4', // contraseña de MySQL
+    database: 'bygribkuoral2phomtxf', // Nombre de la base de datos
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require('express'); 
 const bodyParser = require('body-parser');
 const path = require('path'); // Para resolver las rutas de los archivos estáticos
+const cors = require('cors'); // Importar cors
 const rutaRegistro = require('./routes/rutaRegistro');
 
 const app = express();
 const port = 3000;
+
+// Configuración de CORS
+const corsOptions = {
+    origin: '*', // Permitir acceso desde cualquier origen. Cambia '*' por un dominio específico si es necesario.
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
+
+app.use(cors(corsOptions)); // Aplicar configuración de CORS
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
