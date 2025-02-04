648b296784e10bc596900074c3520a34d5a868ea
YeseniaCarlos <yessecarpe06@gmail.com>
2025-02-04 11:57:10 -0600

    x1

--- model/db.js
+++ model/db.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2/promise');
+/*const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'bygribkuoral2phomtxf-mysql.services.clever-cloud.com',       // dirección de tu servidor MySQL
-    user: 'urddkyk6ocawom8c',      // usuario de MySQL
-    password: 'wRvIpHcxmSAPnTeqL8D4', // contraseña de MySQL
-    database: 'bygribkuoral2phomtxf', // Nombre de la base de datos
+    host: 'localhost',       // Cambia a la dirección de tu servidor MySQL
+    user: 'root',      // Tu usuario de MySQL
+    password: '', // Tu contraseña de MySQL
+    database: 'caltecmex', // Nombre de la base de datos
 });
 
-module.exports = db;
+module.exports = db;*/
+const mysql = require('mysql2');
+
+const conexion = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'caltecmex'
+});
