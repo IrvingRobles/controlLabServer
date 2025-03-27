21d726c2bd59d7771a30b3999aecec8f120164ee
IrvingRobles <dsm21190235.irobles@alumnos.utsv.edu.mx>
2025-03-27 16:34:15 -0600

    commit c1

--- model/db.js
+++ model/db.js
@@ -2,17 +2,10 @@ const { getRounds } = require('bcrypt');
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-<<<<<<< HEAD
     host: 'bf7gchwsnq8qdjrroxwg-mysql.services.clever-cloud.com',       // dirección de tu servidor MySQL
     user: 'uaioyc05nizrr2iw',      // usuario de MySQL
     password: 'pGQtUIYobiEfGZTggEqn', // contraseña de MySQL
     database: 'bf7gchwsnq8qdjrroxwg', // Nombre de la base de datos
-=======
-    host: 'localhost',       // Cambia a la dirección de tu servidor MySQL
-    user: 'root',            // Tu usuario de MySQL
-    password: '',            // Tu contraseña de MySQL
-    database: 'caltecmex2',      // Nombre de la base de datos
->>>>>>> desarrollo-c4
 });
 
 module.exports = db;
