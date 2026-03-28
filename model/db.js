7e2c1030ff8f75a7402f491a8e022c67c4ddce58
IrvingRobles <98172734+IrvingRobles@users.noreply.github.com>
2026-03-28 02:43:43 -0600

    Update db.js

--- model/db.js
+++ model/db.js
@@ -2,10 +2,10 @@ const { getRounds } = require('bcrypt');
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'bgay1ut85jb6meygpwka-mysql.services.clever-cloud.com',       // dirección de tu servidor MySQL
-    user: 'uji1yynbsormfj0c',      // usuario de MySQL
-    password: 'h63OTz2O6w37J2NYvOCI', // contraseña de MySQL
-    database: 'bgay1ut85jb6meygpwka', // Nombre de la base de datos
+    host: 'bwjd5ohuyklwulyyxklo-mysql.services.clever-cloud.com',       // dirección de tu servidor MySQL
+    user: 'u6ieiv1oujeudcll',      // usuario de MySQL
+    password: 'Y0sfZ0jwG2BBUyLsaOXI', // contraseña de MySQL
+    database: 'bwjd5ohuyklwulyyxklo', // Nombre de la base de datos
 });
 
 module.exports = db;
